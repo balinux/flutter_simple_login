1ba71b8fcdfe9d911f62ab0c6363ae1bd4f5b477
balinux <ryojuniyantara@gmail.com>
2019-06-18 08:18:56 +0800

    add profile class and icon, route

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/profile': (context) => Profile(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -64,6 +49,24 @@ class _MyHomePageState extends State<MyHomePage> {
             Image.asset(
               "images/cover.png",
               width: double.maxFinite,
+            ),
+            Positioned(
+              top: 10,
+              right: 10,
+              width: 50,
+              height: 50,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.purple.shade300.withOpacity(.3),
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                  child: Icon(Icons.person_pin,color: Colors.white),
+                )
+              ),
             ),
             Positioned(
               top: 150,
@@ -180,6 +183,20 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Text('Profile page'),
       ),
     );
   }
